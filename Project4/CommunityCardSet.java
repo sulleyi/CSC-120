1a7619cd88388cfc6150c3916bc99b92eed87a9d
sulleyi <57299945+sulleyi@users.noreply.github.com>
2020-06-01 14:11:32 -0400

    Added CommunityCards and StudHand code

---
+++ Project4/CommunityCardSet.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class CommunityCards{
+
+    private ArrayList<Card> communityCards = new ArrayList<Card>(5);
+
+    public CommunityCardSet(ArrayList<Card> cardList){
+        communityCards.addAll(cardList);
+    }
+}
